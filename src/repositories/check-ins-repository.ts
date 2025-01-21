deac0a1101452d294458c86adbee9779c5b56b25
sollandre <andrebmsolla@gmail.com>
2025-01-20 22:57:05 -0300

    Repositories interface definition

---
+++ src/repositories/check-ins-repository.ts
@@ -0,0 +1,11 @@
+import { CheckIn, Prisma } from "@prisma/client";
+
+export interface CheckInsRepository {
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+  save(checkIn: CheckIn): Promise<CheckIn>;
+  countByUserId(userId: string): Promise<number>;
+  findById(id: string): Promise<CheckIn | null>;
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+  readonly MAX_RESULTS_PER_PAGE: 20;
+}
