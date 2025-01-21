7802e2b7606af5a6842438a46160509539e66a63
sollandre <andrebmsolla@gmail.com>
2025-01-20 22:54:50 -0300

    Setup for E2E testing with Prisma and Postgres

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import { Environment } from "vitest/environments";
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "ssr",
+  setup() {
+    //create a database for each test suite
+    //
+
+    const schema = randomUUID();
+
+    console.log(`Setup ${schema}`);
+
+    const prisma = new PrismaClient();
+
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync("npx prisma migrate deploy");
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        );
+
+        await prisma.$disconnect();
+
+        console.log(`Teardown ${schema}`);
+        return;
+      },
+    };
+  },
+};
